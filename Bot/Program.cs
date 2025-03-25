5ad522ad26d1c6c12c3645bbae5f28c30fb640d7
illiaR <ir@readable.games>
2025-03-25 12:27:37 +0200

    Simplify adding new services by di-container

---
+++ Bot/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using PIBScheduleBot;
+using PIBScheduleBot.Services;
+using Telegram.Bot;
+
+var builder = Host.CreateDefaultBuilder()
+    .ConfigureServices((services) =>
+    {
+        var botToken = Environment.GetEnvironmentVariable("BOT_TOKEN");
+
+        if (string.IsNullOrEmpty(botToken))
+        {
+            throw new InvalidOperationException("Bot token is not set!");
+        }
+        
+        Console.WriteLine($"BotToken: {botToken}");
+        
+        services.AddSingleton<ITelegramBotClient>(new TelegramBotClient(botToken));
+        services.AddSingleton<MarkupDrawer>();
+        services.AddSingleton<UpdateHandler>();
+        services.AddHostedService<BotBackgroundService>();
+    });
+
+var host = builder.Build();
+
+await host.RunAsync();

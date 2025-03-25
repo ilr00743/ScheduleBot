5cd19819821bd5e24ef742e21153447ed8631d8a
illiaR <ir@readable.games>
2025-03-25 12:30:57 +0200

    Replaced bot's background process to another class

---
+++ Bot/Services/BotBackgroundService.cs
@@ -0,0 +1,37 @@
+using Microsoft.Extensions.Hosting;
+using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types.Enums;
+
+namespace PIBScheduleBot.Services;
+
+public class BotBackgroundService : BackgroundService
+{
+    private readonly ITelegramBotClient _botClient;
+    private readonly UpdateHandler _updateHandler;
+
+    public BotBackgroundService(ITelegramBotClient botClient, UpdateHandler updateHandler)
+    {
+        _botClient = botClient;
+        _updateHandler = updateHandler;
+    }
+    
+    protected override async Task ExecuteAsync(CancellationToken cancellationToken)
+    {
+        var receiverOptions = new ReceiverOptions
+        { 
+            AllowedUpdates = new UpdateType[] { UpdateType.Message, UpdateType.CallbackQuery},
+            DropPendingUpdates = true
+        };
+        
+        _botClient.StartReceiving(
+            _updateHandler.HandleUpdateAsync,
+            _updateHandler.HandleErrorAsync,
+            receiverOptions,
+            cancellationToken);
+        
+        Console.WriteLine("Bot has been launched.");
+
+        await Task.Delay(-1, cancellationToken);
+    }
+}
